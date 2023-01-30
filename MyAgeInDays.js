71ec869d68a449631f908d80376715923b77b603
Di <hanwi1994@gmail.com>
2023-01-30 16:25:56 +0100

    Even neater. 4x shorter, without excesssive object

--- MyAgeInDays.js
+++ MyAgeInDays.js
@@ -3,45 +3,28 @@ function ageInDays() {
   const dateOfBirth = new Date(dateOfBirthTag.value);
   date = new Date();
   const dayToday = date.getDate();
-  const monthTodayInt = date.getMonth() + 1;
+  const monthToday = date.getMonth();
   const currentYear = date.getFullYear();
   const myYearOfBirth = dateOfBirth.getFullYear();
-  const myMonthOfBirthInt = dateOfBirth.getMonth() + 1;
+  const myMonthOfBirth = dateOfBirth.getMonth();
   const myDayOfBirth = dateOfBirth.getDate();
-  // Parses months numbers to string
   const months = {
-    1: { name: "jan", days: 31 },
-    2: { name: "feb", days: 28 },
-    3: { name: "mar", days: 31 },
-    4: { name: "apr", days: 30 },
-    5: { name: "may", days: 31 },
-    6: { name: "jun", days: 30 },
-    7: { name: "jul", days: 31 },
-    8: { name: "aug", days: 31 },
-    9: { name: "sep", days: 30 },
-    10: { name: "oct", days: 31 },
-    11: { name: "nov", days: 30 },
-    12: { name: "dec", days: 31 },
+    0: { days: 31, monthYearDays: 31, prevMonthDays: 0 },
+    1: { days: 28, monthYearDays: 59, prevMonthDays: 31 },
+    2: { days: 31, monthYearDays: 90, prevMonthDays: 59 },
+    3: { days: 30, monthYearDays: 120, prevMonthDays: 90 },
+    4: { days: 31, monthYearDays: 151, prevMonthDays: 120 },
+    5: { days: 30, monthYearDays: 181, prevMonthDays: 151 },
+    6: { days: 31, monthYearDays: 212, prevMonthDays: 181 },
+    7: { days: 31, monthYearDays: 243, prevMonthDays: 212 },
+    8: { days: 30, monthYearDays: 273, prevMonthDays: 243 },
+    9: { days: 31, monthYearDays: 304, prevMonthDays: 273 },
+    10: { days: 30, monthYearDays: 334, prevMonthDays: 304 },
+    11: { days: 31, monthYearDays: 365, prevMonthDays: 334 },
   };
-  const monthToday = months[monthTodayInt].name;
-  const daysInMonth = months[myMonthOfBirthInt].days;
-  const myMonthOfBirth = months[myMonthOfBirthInt].name;
-  const monthYearDays = {
-    jan: 31,
-    feb: 59,
-    mar: 90,
-    apr: 120,
-    may: 151,
-    jun: 181,
-    jul: 212,
-    aug: 243,
-    sep: 273,
-    oct: 304,
-    nov: 334,
-    dec: 365,
-  };
-
-  let leftDays = monthYearDays[myMonthOfBirth];
+  const daysInMonth = months[myMonthOfBirth].days;
+  const leftDays = months[myMonthOfBirth].monthYearDays;
+  const prevMonthsDays = months[monthToday].prevMonthDays;
 
   let daysInYearOfBirth = 366;
   if (myYearOfBirth % 4) {
@@ -50,22 +33,7 @@ function ageInDays() {
   const myDaysInBornYear = daysInYearOfBirth - leftDays + daysInMonth - myDayOfBirth;
   const myDaysInBetweenYears =
     (currentYear - myYearOfBirth - 1) * 365 + Math.floor((currentYear - myYearOfBirth + 1) / 4);
-
-  const prevMonthDays = {
-    jan: 0,
-    feb: 31,
-    mar: 59,
-    apr: 90,
-    may: 120,
-    jun: 151,
-    jul: 181,
-    aug: 212,
-    sep: 243,
-    oct: 273,
-    nov: 304,
-    dec: 334,
-  };
-  const DaysInCurrentYear = prevMonthDays[monthToday] + dayToday; //(jan)0+26
+  const DaysInCurrentYear = prevMonthsDays + dayToday; //(jan)0+26
   const myDays = myDaysInBornYear + myDaysInBetweenYears + DaysInCurrentYear;
   result = document.getElementById("result"); //here we receive result from html without value
   result.value = myDays; //we put  this value in "result"
